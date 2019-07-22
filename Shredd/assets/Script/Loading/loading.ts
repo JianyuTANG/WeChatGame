17b1b5c55dde30639cf48cbf939ddfbe8c83d131
jianyuTANG <jianyutang99@gmail.com>
2019-07-22 10:14:56 +0800

    add subPackage

--- Shredd/assets/Script/Loading/loading.ts
+++ Shredd/assets/Script/Loading/loading.ts
@@ -8,7 +8,7 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
@@ -19,14 +19,33 @@ export default class NewClass extends cc.Component {
     @property
     text: string = 'hello';
 
+    @property(cc.Node)
+    bg: cc.Node = null;
+
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
+        this.matchScreen();
         //分包加载
     }
 
-    start () {
-        cc.director.loadScene('Start');
+    private matchScreen() {
+        this.bg.width = cc.winSize.width;
+        this.bg.height = cc.winSize.height;
+    }
+
+    start() {
+        this.loadPackage();
+    }
+
+    private loadPackage() {
+        cc.loader.downloader.loadSubpackage('Texture', function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('load subpackage successfully.');
+            cc.director.loadScene('Start');
+        });
     }
 
     // update (dt) {}
